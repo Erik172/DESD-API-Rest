8fd198840e986ac0985d30063fed1b228a5dd66c
zer0 <erik172dev@gmail.com>
2024-04-06 19:24:36 -0500

    Refactor Flask imports and update resource import

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,10 @@
 from flask import Flask
-from flask_restful import Api, Resource
-from dotenv import load_dotenv
+from flask_restful import Api
 
-from resources.black_documents import BlackDocumentsDetectorModel
-
-load_dotenv()
+from resources import BlackDocumentsDetectorModel
 
 app = Flask(__name__)
 api = Api(app)
-    
-class TiltedDocumentsDetectorOCRScript(Resource):
-    pass
             
 api.add_resource(BlackDocumentsDetectorModel, "/black-documents-detector")
 
