5367b591ae15d595653fa1262c30494f2624ecc8
zer0 <erik172dev@gmail.com>
2024-04-06 19:11:56 -0500

    Add main.py with page links

---
+++ frontend/main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import requests
+import os
+
+st.page_link('main.py', label='Home', icon='🏠')
+st.page_link('pages/black-document-detector.py', label='Black Document Detector', icon='📄')
