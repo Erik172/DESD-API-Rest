1e12eedbdd1eeb3d45dbba1afd2e5a844bb40138
zer0 <erik172dev@gmail.com>
2024-04-08 21:15:39 -0500

    Refactor resource imports and add page link for TilDe in main.py

--- frontend/main.py
+++ frontend/main.py
@@ -2,5 +2,6 @@ import streamlit as st
 import requests
 import os
 
-st.page_link('main.py', label='Home', icon='🏠')
+# st.page_link('main.py', label='Home', icon='🏠')
+st.page_link('pages/tilde.py', label='TilDe (Tilted Detection)', icon='📐')
 st.page_link('pages/black-document-detector.py', label='Black Document Detector', icon='📄')
