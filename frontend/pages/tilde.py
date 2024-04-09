049710f994ea507ac8b01489c30fc5e335046454
zer0 <erik172dev@gmail.com>
2024-04-08 21:16:29 -0500

    Refactor code for processing uploaded images and PDF files

---
+++ frontend/pages/tilde.py
@@ -0,0 +1,103 @@
+from pdf2image import convert_from_bytes
+import streamlit as st
+import requests
+import os
+
+API_URL_BASE = "http://localhost:5000/tilde"
+
+st.set_page_config(
+    page_title="TilDe (Tilted Detection)",
+    page_icon="🤖",
+    layout="centered",
+    initial_sidebar_state="auto"
+)
+
+def process_uploaded_images(uploaded_file, show_image, version="v1"):
+    with st.spinner("Processing..."):
+        for file in uploaded_file:
+            image = file.read()
+            API_URL = f"{API_URL_BASE}/{version}"
+            response = requests.post(API_URL, files={"image": image})
+            response = response.json()
+
+            st.caption(file.name)
+
+            if version == "v1":
+                st.progress(response['data'][0]['confidence'], f"{response['data'][0]['name']}, {round(response['data'][0]['confidence'], 3)}")
+                st.progress(response['data'][1]['confidence'], f"{response['data'][1]['name']}, {round(response['data'][1]['confidence'], 3)}")
+
+                prediction, confidence = st.columns(2)
+                prediction.metric("Prediction", response['data'][0]['name'])
+                confidence.metric("Confidence", round(response['data'][0]['confidence'], 4))
+
+            if version == "v2":
+                prediction, confidence, time = st.columns(3)
+                prediction.metric("Prediction", response["top"])
+                confidence.metric("Confidence", response["confidence"])
+                time.metric("Time", round(response["time"], 3), "seconds")
+
+            
+            if show_image:
+                st.image(image, use_column_width=True, caption="Uploaded Image")
+            print(response)
+            st.divider()
+
+def process_pdf_file(uploaded_file, show_image, version="v1"):
+    with st.spinner("Processing..."):
+        images = convert_from_bytes(uploaded_file.read())
+        for i, image in enumerate(images):
+            image.save(f"temp_{i}.jpg")
+            image_path = f"temp_{i}.jpg"
+            image = open(f"temp_{i}.jpg", "rb")
+            API_URL = f"{API_URL_BASE}/{version}"
+            response = requests.post(API_URL, files={"image": image})
+            response = response.json()
+
+            st.caption(f"Page {i + 1}")
+
+            if version == "v1":
+                st.progress(response['data'][0]['confidence'], f"{response['data'][0]['name']}, {round(response['data'][0]['confidence'], 3)}")
+                st.progress(response['data'][1]['confidence'], f"{response['data'][1]['name']}, {round(response['data'][1]['confidence'], 3)}")
+
+                prediction, confidence = st.columns(2)
+                prediction.metric("Prediction", response['data'][0]['name'])
+                confidence.metric("Confidence", round(response['data'][0]['confidence'], 4))
+
+            if version == "v2":
+                prediction, confidence, time = st.columns(3)
+                prediction.metric("Prediction", response["top"])
+                confidence.metric("Confidence", response["confidence"])
+                time.metric("Time", round(response["time"], 3), "seconds")
+
+            if show_image:
+                st.image(image_path, use_column_width=True, caption="Uploaded Image", output_format="JPEG")
+            
+            try:
+                os.remove(f"temp_{i}.jpg")
+            except PermissionError:
+                print("PermissionError: Unable to delete the temporary file.")
+    
+            st.divider()
+
+def main():
+    st.title("TilDe (Tilted Detection) 📐")
+    st.markdown("This page allows you to detect tilted documents using the TilDe model.")
+    st.markdown("**V1:** __YOLOv8n-CLS (Ultralytics)__ - This version of the model offers better performance, but it has a more complex architecture and slower inference time.")
+    st.markdown("**V2:** __MobileNetV2 (Roboflow)__ - This version of the model is smaller, resulting in faster inference time, but it may have lower performance compared to V1.")
+
+    version = st.selectbox(
+        "Select the version of the model to use",
+        ("v1", "v2")
+    )
+    
+    show_image = st.checkbox("Show uploaded image(s)", value=True)
+    uploaded_file = st.file_uploader("Upload image(s)", type=["jpg", "jpeg", "png", "tif", "tiff"], accept_multiple_files=True)
+    uploaded_pdf = st.file_uploader("Upload PDF file", type=["pdf"], accept_multiple_files=False)
+    
+    if uploaded_file:
+        process_uploaded_images(uploaded_file, show_image, version)
+    if uploaded_pdf:
+        process_pdf_file(uploaded_pdf, show_image, version)
+
+if __name__ == "__main__":
+    main()
