c47bd53bf08815ff098c3945ac478b3c12b01a75
zer0 <erik172dev@gmail.com>
2024-04-21 20:27:33 -0500

    Fix typo in debug flag value in main.py

--- backend/main.py
+++ backend/main.py
@@ -17,4 +17,4 @@ api.add_resource(RoDeV1, "/rode/v1")
 api.add_resource(Audit, "/audit")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=Fa, host="0.0.0.0", port=5000)
