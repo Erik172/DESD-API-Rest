1031e59cf4695586be9541d477f9dab687fb7979
zer0 <erik172dev@gmail.com>
2024-05-03 22:35:55 -0500

    Refactor file handling and add OCR support in hoja_control function

--- frontend/resources/filters.py
+++ frontend/resources/filters.py
@@ -1,7 +1,9 @@
-from PIL import Image
 import pytesseract
+import numpy as np
+import cv2
 
-def hoja_control(image):
-    image = Image.open(image)
+def hoja_control(image) -> bool:
+    if type(image) == bytes:
+        image = cv2.imdecode(np.frombuffer(image, np.uint8), cv2.IMREAD_COLOR)
     text = pytesseract.image_to_string(image)
     return "hoja de control" in text[:44].lower()
