3553d093dd09372745926faa62c64ec67889ac21
zer0 <erik172dev@gmail.com>
2024-05-03 21:40:47 -0500

    Refactor file handling in Audit endpoint

--- backend/resources/audit.py
+++ backend/resources/audit.py
@@ -2,6 +2,7 @@ from flask import request, jsonify
 from flask_restful import Resource
 from ultralytics import YOLO
 import base64
+import random
 
 from src import parse_result_yolov8
 
@@ -20,20 +21,21 @@ class Audit(Resource):
         image = request.files["image"]
         image = image.read()
         image = base64.b64encode(image)
-        with open('temp.png', 'wb') as f:
+        file_name = str(random.choices('abcdefghijklmnopqrstuvwxyz', k=10)) + '.png'
+        with open(file_name, 'wb') as f:
             f.write(base64.b64decode(image))
 
         tilde_model = YOLO(models["tilde_v1"])
         rode_model = YOLO(models["rode_v1"])
         cude_model = YOLO(models["cude_v1"])
 
-        tilde_results = tilde_model("temp.png")
+        tilde_results = tilde_model(file_name)
         tilde_results = parse_result_yolov8(tilde_results[0])
 
-        rode_results = rode_model("temp.png")
+        rode_results = rode_model(file_name)
         rode_results = parse_result_yolov8(rode_results[0])
 
-        cude_results = cude_model("temp.png")
+        cude_results = cude_model(file_name)
         cude_results = parse_result_yolov8(cude_results[0])
 
         results["tilde"] = tilde_results['data'][0]
