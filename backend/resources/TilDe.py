8ecf86454a3e78088910cc8a8d03368204ab92af
zer0 <erik172dev@gmail.com>
2024-05-08 20:57:28 -0500

    chore: Refactor file handling, add new code, and update dependencies

--- backend/resources/TilDe.py
+++ backend/resources/TilDe.py
@@ -8,7 +8,9 @@ import sentry_sdk.metrics
 from ultralytics import YOLO
 from datetime import datetime
 
-from src import parse_result_yolov8
+from src import parse_result_yolov8, data_file_validation
+from src.filters import apply_filters
+from db import Work
 
 class TilDeV1(Resource):
     def post(self):
@@ -21,6 +23,9 @@ class TilDeV1(Resource):
             if "image" not in request.files:
                 return {"error": "No file uploaded"}
             
+            request.form = data_file_validation(request)
+            work = Work(request.form["work_id"])
+            
             image = request.files["image"]
             image = image.read()
             image = base64.b64encode(image)
@@ -31,6 +36,19 @@ class TilDeV1(Resource):
             response = model(file_name)
             response = parse_result_yolov8(response[0])
             response['time'] = (datetime.now() - start_time).total_seconds()
+            response['model'] = "TilDeV1"
+
+            response = apply_filters(file_name=file_name, requests=request.form, response=response)
+
+            r_form = request.form.to_dict()
+            r_form.pop("filtros")
+            # unir response y request.form
+            documento = r_form | response
+            documento['prediccion'] = response['data'][0]['name']
+            documento['confianza'] = response['data'][0]['confidence']
+            documento.pop("data")
+            doc_id = work.save(documento)
+            response["_id"] = str(doc_id)
 
             sentry_sdk.metrics.incr(
                 key="TilDeV1Count",
@@ -38,5 +56,4 @@ class TilDeV1(Resource):
             )
 
             os.remove(file_name)
-
             return jsonify(response)
