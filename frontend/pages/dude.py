c607a012a50db36558615414be8caa93d26967c7
zer0 <erik172dev@gmail.com>
2024-05-15 09:17:29 -0500

    chore: Refactor file handling and update frontend page for DuDe image processing

--- frontend/pages/dude.py
+++ frontend/pages/dude.py
@@ -9,29 +9,31 @@ st.set_page_config(
     initial_sidebar_state="auto"
 )
 
-st.warning("En Desarrollo ⚠️")
 st.title("DuDe (Duplicate Detection) Detección de duplicados 2️⃣")
-    
+st.warning("En construcción... 🚧")
+
+work_id = st.text_input("Identificador de trabajo", placeholder=f"Identificador de trabajo (Opcional)")
+
 show_image = st.checkbox("Mostrar imagenes", value=False)
 uploaded_file = st.file_uploader("Upload image(s)", type=["jpg", "jpeg", "png", "tif", "tiff"], accept_multiple_files=True)
 
 
-def process_uploaded_images(uploaded_file, show_image):
+def process_uploaded_images(uploaded_file, folder_name: str = "dude", show_image: bool = False):
     if len(uploaded_file) < 2:
         st.error("Debes cargar al menos dos imágenes")
         
     else:
         with st.spinner(f"subiendo {len(uploaded_file)} imágenes..."):
-            bar_progress = st.progress(0, text="Subiendo...")
-            url = "http://localhost:5000/dude/v1/dude"
+            bar_progress = st.progress(0, text="Subiendo archivos...")
+            url = f"http://localhost:5000/dude/{folder_name}"
             for file in uploaded_file:
-                bar_progress.progress(uploaded_file.index(file) / len(uploaded_file), f"Subiendo {uploaded_file.index(file) + 1}/{len(uploaded_file)}")
+                bar_progress.progress(uploaded_file.index(file) / len(uploaded_file), f"Subiendo {uploaded_file.index(file) + 1}/{len(uploaded_file)} archivos...")     
                 response = requests.post(url, files={"file": file})
                 st.toast(response.json()["message"], icon="🎈")
-            bar_progress.progress(100, "Subiendo completado")
+            bar_progress.progress(100, "Subida de archivos completada 🎈")
         
         with st.spinner("Buscando duplicados..."):
-            url = "http://localhost:5000/dude/v1/dude"
+            url = f"http://localhost:5000/dude/{folder_name}"
             response = requests.get(url).json()
 
             if response:
@@ -44,19 +46,24 @@ def process_uploaded_images(uploaded_file, show_image):
 
         if response.get('duplicates'):
             duplicates = response.get('duplicates')
-            st.warning(f":warning: {len(duplicates)} duplicados encontrados")
+            st.warning(f":warning: {len(duplicates)} archivos con duplicados encontrados")
             for key, value in duplicates.items():
-                st.write(f"Archivo: {key}")
-                for file in value:
-                    st.write(f"Duplicado: {file}")
+                with st.expander(f"Archivo: {key}"):
+                    st.write(f'Numero de duplicados: {len(value)}')
+                    for i, duplicate in enumerate(value):
+                        st.write(f'{i + 1}. {duplicate}')
 
         else:
             st.success("No se encontraron duplicados", icon="✅")
 
 def main():
+    global work_id
+    if not work_id:
+        work_id = f"dude_{datetime.now().strftime('%Y%m%d%H%M%S')}"
+
     if st.button("Buscar Duplicados", help="Presiona el botón para procesar los archivos cargados", use_container_width=True):
         if uploaded_file:
-            process_uploaded_images(uploaded_file, show_image)
+            process_uploaded_images(uploaded_file, work_id)
 
 if __name__ == "__main__":
     main()
