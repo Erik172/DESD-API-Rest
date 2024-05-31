57c6273cc27da2a5d40000148f133a734a378b4b
zer0 <erik172dev@gmail.com>
2024-05-31 00:24:14 -0500

    Refactor image processing code and error handling in frontend pages

--- backend/resources/TilDe.py
+++ backend/resources/TilDe.py
@@ -1,59 +1,60 @@
-import os
-import base64
-import sentry_sdk
-import random
 from flask import request, jsonify
 from flask_restful import Resource
-import sentry_sdk.metrics
 from ultralytics import YOLO
 from datetime import datetime
+import random
+import os
 
 from src import parse_result_yolov8
 from src.filters import apply_filters
+
 from db import Work
 
 class TilDe(Resource):
+    def get(self):
+        return {"message": "TilDeV1"}, 200
+    
     def post(self):
-        with sentry_sdk.metrics.timing(key="TilDeV1", tags={"model": "TilDeV1"}):
-            start_time = datetime.now()
-            model_path = 'models/tilde_v1.pt'
+        
+        start_time = datetime.now()
+        model_path = 'models/tilde_v1.pt'
             
-            model = YOLO(model_path, verbose=True)
+        model = YOLO(model_path, verbose=True)
 
-            if "image" not in request.files:
-                return {"error": "No image found in request"}, 400
+        if "image" not in request.files:
+            return {"error": "No image found in request"}, 400
             
-            request.form = request.form.to_dict()
+        request.form = request.form.to_dict()
 
-            if not request.form.get("work_id"):
-                request.form["work_id"] = "tilde_test"
+        if not request.form.get("work_id"):
+            request.form["work_id"] = "tilde_test"
 
-            work = Work(request.form["work_id"])
+        work = Work(request.form["work_id"])
             
-            file_name = f'temp/{"".join(random.choices("abcdefghijklmnopqrstuvwxyz", k=10))}.jpg'
-            request.files["image"].save(file_name)
+        file_name = f'temp/{"".join(random.choices("abcdefghijklmnopqrstuvwxyz", k=10))}.jpg'
+        request.files["image"].save(file_name)
 
-            response = model(file_name)
-            response = parse_result_yolov8(response[0])
-            response['time'] = (datetime.now() - start_time).total_seconds()
-            response['model'] = "TilDeV1"
+        response = model(file_name)
+        response = parse_result_yolov8(response[0])
+        response['time'] = (datetime.now() - start_time).total_seconds()
+        response['model'] = "TilDeV1"
 
-            response = apply_filters(file_name=file_name, requests=request.form, response=response)
+        response = apply_filters(file_name=file_name, requests=request.form, response=response)
 
-            if request.form.get("filtros"):
-                request.form.pop("filtros")
-            # unir response y request.form
-            documento = request.form | response
-            documento['prediccion'] = response['data'][0]['name']
-            documento['confianza'] = response['data'][0]['confidence']
-            documento.pop("data")
-            doc_id = work.save(documento)
-            response["_id"] = str(doc_id)
+        if request.form.get("filtros"):
+            request.form.pop("filtros")
 
-            sentry_sdk.metrics.incr(
-                key="TilDeV1Count",
-                tags={"model": "TilDeV1"}
-            )
+        # unir response y request.form
+        documento = request.form | response
+        documento['prediccion'] = response['data'][0]['name']
+        documento['confianza'] = response['data'][0]['confidence']
+        documento.pop("data")
+        doc_id = work.save(documento)
+        response["_id"] = str(doc_id)
 
+        try:
             os.remove(file_name)
-            return jsonify(response)
+        except PermissionError:
+            print(f"Error al eliminar el archivo {file_name}")
+
+        return jsonify(response)
