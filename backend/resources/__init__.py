215b46633f8799b4ab69d1f44c26434b913aee41
zer0 <erik172dev@gmail.com>
2024-06-05 12:43:33 -0500

    refactor: Update imports for DuDe and DESD in __init__.py

--- backend/resources/__init__.py
+++ backend/resources/__init__.py
@@ -1,5 +1,5 @@
 from .resultados import Resultados
 from .export import Export
 from .model_ai import ModelAI
-from .dude import DuDe
-from .desd import DESD
+from .DuDe import DuDe
+from .DESD import DESD
