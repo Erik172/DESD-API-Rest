fef004781a922cb10062ca9ead8fe5edcf1bb9b6
zer0 <erik172dev@gmail.com>
2024-06-04 20:49:21 -0500

    refactor: Update page title and variable names in resultados.py

--- frontend/pages/resultados.py
+++ frontend/pages/resultados.py
@@ -1,31 +1,27 @@
 import requests
 import streamlit as st
-from datetime import datetime
 
-st.title("Resultados 📁")
+st.title("Trabajos 📁")
 
-works = requests.get("http://localhost:5000/works").json()
+resultados = requests.get("http://localhost:5000/resultados").json()
 
-# crear por cada work un expander
-for work in works:
-    with st.expander(f"Trabajo: **{work}**", expanded=False):
-        if st.button(f'Cargar datos de exportación para el trabajo {work}'):
-            data = requests.get(f"http://localhost:5000/work/{work}/export").json()
-            st.caption(f'Fecha de creación del CSV: **{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}**')
+for resultado in resultados:
+    with st.expander(f"Resultado: **{resultado}**", expanded=False):
+        if st.button(f'Cargar datos de exportación para el trabajo {resultado}'):
+            data = requests.get(f"http://localhost:5000/export/{resultado}").json()
             st.write(f'Imágenes procesadas: **{data["total"]}**')
 
             with st.container():
                 st.download_button(
                     label="Descargar CSV",
                     data=requests.get(f"http://localhost:5000{data['url']}").content,
-                    file_name=f"{work}.csv",
+                    file_name=f"{resultado}.csv",
                     mime="text/csv"
                 )
 
-        if st.button(f"Eliminar trabajo {work}"):
-            requests.delete(f"http://localhost:5000/works/{work}")
-            requests.delete(f"http://localhost:5000/work/{work}/export")
-            st.success(f"Trabajo {work} eliminado con éxito.")
-            st.toast("Trabajo eliminado con éxito.", icon="✅")
-            # recargar la página
+        if st.button(f"Eliminar Resultado {resultado}"):
+            requests.delete(f"http://localhost:5000/resultados/{resultado}")
+            requests.delete(f"http://localhost:5000/export/{resultado}")
+            st.success(f"Resultado {resultado} eliminado con éxito.")
+            st.toast("Resultado eliminado con éxito.", icon="✅")
             st.experimental_rerun()
