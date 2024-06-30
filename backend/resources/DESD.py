c344f58258304e88d45c86bf75477fda178d1920
zer0 <erik172dev@gmail.com>
2024-06-29 23:28:53 -0500

    refactor: Update ModelAI initialization and prediction methods

--- backend/resources/DESD.py
+++ backend/resources/DESD.py
@@ -5,6 +5,7 @@ from flask_restful import Resource
 from flask import request
 from src import save_results
 from datetime import datetime
+import yaml
 import os
 
 from services import ModelAI
@@ -12,127 +13,79 @@ from database import WorkStatus, sql_db
 
 
 class DESD(Resource):
-    """
-    DESD (Detection Error on Scan Documents) class for handling document scanning and prediction.
-
-    This class provides a POST method to handle file uploads, perform predictions using specified models,
-    and save the results.
-
-    Attributes:
-        None
-
-    Methods:
-        post: Handles the POST request for file upload, prediction, and result saving.
-
-    """
     def get(self):
-            """
-            Retrieves a list of models from the 'models.yml' file.
-
-            Returns:
-                A tuple containing the list of models and the HTTP status code 200.
-            """
-            import yaml
-
             with open('models.yml') as file:
-                models = yaml.full_load(file)
-
-            models = list(models.keys())
+                models = list(yaml.safe_load(file).keys())
 
             return models, 200
 
     def post(self):
-        if 'result_id' not in request.form:
-             return {"message": "No result_id selected"}, 400
-        
-        if 'models' not in request.form:
-            return {"message": "No models selected"}, 400
+        required_fields = ['result_id', 'models', 'files']
+        for field in required_fields:
+            if field not in request.form and field != 'files':
+                return {"message": f"No {field} provided"}, 400
+            elif field == 'files' and field not in request.files:
+                return {"message": "No file part in the request"}, 400
 
-        if 'files' not in request.files:
-            return {"message": "No file part in the request"}, 400
-        
         model_names = request.form.getlist('models')
         files = request.files.getlist('files')
         result_id = request.form.get('result_id')
 
+        work_status = self._create_work_status(result_id, len(files), model_names)
+
+        models = {model: ModelAI(model) for model in model_names if model in ['inclinacion', 'rotacion', 'corte_informacion']}
+
+        for file in files:
+            filename = secure_filename(file.filename)
+            file_path = os.path.join('temp', filename)
+            file.save(file_path)
+
+            results = {filename: {}}
+
+            if filename.lower().endswith('.pdf'):
+                self._pdf_process(models, results, filename, file_path)
+            elif filename.lower().endswith(('.tiff', '.tif')):
+                self._tiff_process(models, results, filename, file_path)
+            else:
+                self._process_image(models, results, filename, file_path)
+
+            self._cleanup_file(file_path)
+
+            if not save_results(results, result_id):
+                work_status.status = 'failed'
+                sql_db.session.commit()
+                return {"message": "Failed to save results"}, 500
+
+            self._update_work_status(work_status)
+
+        work_status.status = 'completed'
+        sql_db.session.commit()
+
+        return {"message": "Processing completed successfully"}, 200
+
+    def _create_work_status(self, result_id, total_files, model_names):
         work_status = WorkStatus(
             result_id=result_id,
             status='in_progress',
             files_processed=0,
-            total_files=len(files),
-            percentage=0
+            total_files=total_files,
+            percentage=0,
+            tilted='inclinacion' in model_names,
+            rotation='rotacion' in model_names,
+            cut_information='corte_informacion' in model_names
         )
-
-        if 'inclinacion' in model_names:
-            work_status.tilted = True
-        if 'rotacion' in model_names:
-            work_status.rotation = True
-        if 'corte_informacion' in model_names:
-            work_status.cut_information = True
-
         sql_db.session.add(work_status)
         sql_db.session.commit()
-    
-        for file in files:
-            results = {}
-            filename = secure_filename(file.filename)
-            file.save('temp/' + filename)
-            results[filename] = {}
+        return work_status
 
-            ### PDF
-            if filename.lower().endswith('.pdf'):
-                self._pdf_process(model_names, results, filename)
-
-            ### TIFF
-            elif filename.lower().endswith('.tiff') or filename.lower().endswith('.tif'):
-                self._tiff_process(model_names, results, filename)
-            
-            ### Image
-            else:
-                for model_name in model_names:
-                    model = ModelAI(model_name)
-                    model_results = model.predict('temp/' + filename)
-                    results[filename][model_name] = {
-                        '0': {
-                            'prediccion': model_results['data'][0]['name'],
-                            'confianza': model_results['data'][0]['confidence'],
-                            'tiempo(s)': model_results['time']
-                        }
-                    }
-
-            try:
-                os.remove('temp/' + filename)
-            except PermissionError:
-                pass
-
-            if save_results(results, result_id) == False:
-                work_status.status = 'failed'
-                sql_db.session.commit()
-
-            work_status.files_processed += 1
-            work_status.percentage = (work_status.files_processed / work_status.total_files) * 100
-            sql_db.session.commit()
-        
-        work_status.status = 'completed'
+    def _update_work_status(self, work_status):
+        work_status.files_processed += 1
+        work_status.percentage = (work_status.files_processed / work_status.total_files) * 100
         sql_db.session.commit()
 
-        return {"message": "Results saved successfully", "result_id": result_id}, 200
-
-    def _tiff_process(self, model_names: list, results: dict, filename: str):
-        """
-        Process a TIFF image by converting each page to JPEG and making predictions using the specified models.
-
-        Args:
-            model_names (list): A list of model names to use for prediction.
-            results (dict): A dictionary to store the prediction results.
-            filename (str): The name of the TIFF file to process.
-
-        Returns:
-            None
-        """
-        tiff_image = Image.open('temp/' + filename)
-        for model_name in model_names:
-            model = ModelAI(model_name)
+    def _tiff_process(self, models, results, filename, file_path):
+        tiff_image = Image.open(file_path)
+        for model_name, model in models.items():
             results[filename][model_name] = {}
             for i, page in enumerate(ImageSequence.Iterator(tiff_image)):
                 jpg_file_path = f"temp/{filename}_{i}.jpg"
@@ -143,25 +96,38 @@ class DESD(Resource):
                     'confianza': model_results['data'][0]['confidence'],
                     'tiempo(s)': model_results['time']
                 }
-                try:
-                    os.remove(jpg_file_path)
-                except PermissionError:
-                    pass
+                self._cleanup_file(jpg_file_path)
 
-    def _pdf_process(self, model_names, results, filename):
-        images = convert_from_path('temp/' + filename)
-        for model_name in model_names:
-            model = ModelAI(model_name)
+    def _pdf_process(self, models, results, filename, file_path):
+        images = convert_from_path(file_path, thread_count=os.cpu_count())
+        for model_name, model in models.items():
             results[filename][model_name] = {}
             for i, image in enumerate(images):
-                image.save('temp/' + filename + f'_{i}.png')
-                model_results = model.predict('temp/' + filename + f'_{i}.png')
+                image.save(f'temp/{filename}_{i}.png')
+                model_results = model.predict(f'temp/{filename}_{i}.png')
                 results[filename][model_name][str(i)] = {
-                            'prediccion': model_results['data'][0]['name'],
-                            'confianza': model_results['data'][0]['confidence'],
-                            'tiempo(s)': model_results['time']
-                        }
-                os.remove('temp/' + filename + f'_{i}.png')
+                    'prediccion': model_results['data'][0]['name'],
+                    'confianza': model_results['data'][0]['confidence'],
+                    'tiempo(s)': model_results['time']
+                }
+                self._cleanup_file(f'temp/{filename}_{i}.png')
+
+    def _process_image(self, models, results, filename, file_path):
+        for model_name, model in models.items():
+            model_results = model.predict(file_path)
+            results[filename][model_name] = {
+                '0': {
+                    'prediccion': model_results['data'][0]['name'],
+                    'confianza': model_results['data'][0]['confidence'],
+                    'tiempo(s)': model_results['time']
+                }
+            }
+
+    def _cleanup_file(self, file_path):
+        try:
+            os.remove(file_path)
+        except PermissionError:
+            print(f"Unable to remove file: {file_path}")
 
 class DESDStatus(Resource):
     def get(self, result_id: str):
--- backend/services/model_ai.py
+++ backend/services/model_ai.py
@@ -5,59 +5,47 @@ import platform
 import yaml
 
 class ModelAI:
-    """
-    Represents a ModelAI object that can be used for prediction tasks.
+    def __init__(self, model_name: str, task: str = 'classify') -> None:
+        config_file = 'models.yml' if self._is_intel() else 'models.yml'
+        try:
+            with open(config_file) as file:
+                config = yaml.safe_load(file)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Configuration file '{config_file}' not found.")
 
-    Attributes:
-        model (str): The name of the model being used.
-        results (dict): A dictionary containing the predicted output, execution time, and model information.
-
-    Methods:
-        __init__(self, model_name: str) -> None: Initializes a ModelAI object.
-        predict(self, image_path: str, task: str = 'classify') -> dict: Predicts the output for the given image.
-        _is_intel(self) -> bool: Checks if the processor is from Intel.
-    """
-
-    def __init__(self, model_name: str) -> None:
-        """
-        Initializes a ModelAI object.
-
-        Args:
-            model_name (str): The name of the model to be used.
-
-        Raises:
-            FileNotFoundError: If the configuration file is not found.
-        """
-        if self._is_intel():
-            with open('models_openvino.yml') as file:
-                config = yaml.load(file, Loader=yaml.FullLoader)
-        else:
-            with open('models.yml') as file:
-                config = yaml.load(file, Loader=yaml.FullLoader)
-
-        self.model = config[model_name]
+        self.model_path = config.get(model_name)
+        if not self.model_path:
+            raise ValueError(f"Model '{model_name}' not found in configuration.")
+        
+        self.model = YOLO(self.model_path, verbose=True, task=task)
         self.results = {}
 
-    def predict(self, image_path: str, task: str = 'classify') -> dict:
+    def predict(self, image_path: str) -> dict:
         """
-        Predicts the output for the given image using the YOLO model.
+        Predicts the output for the given image path using the model.
 
         Args:
             image_path (str): The path to the input image.
-            task (str, optional): The task to perform. Defaults to 'classify'.
 
         Returns:
-            dict: A dictionary containing the predicted output, execution time, and model information.
+            dict: A dictionary containing the prediction results, including the parsed result,
+                  execution time, and model path.
         """
         start_time = datetime.now()
-        model = YOLO(self.model, verbose=True, task=task)
-        response = parse_result_yolov8(model(image_path)[0])
-        response['time'] = (datetime.now() - start_time).total_seconds()
-        response['model'] = self.model
-        self.results = response
-        return response
+        result = self.model(image_path)[0]
+        self.results = {
+            **parse_result_yolov8(result),
+            'time': (datetime.now() - start_time).total_seconds(),
+            'model': self.model_path
+        }
+        return self.results
 
-    def _is_intel(self) -> bool:
-        if 'intel' in platform.processor().lower():
-            return True
-        return False
+    @staticmethod
+    def _is_intel() -> bool:
+        """
+        Check if the current processor is from Intel.
+
+        Returns:
+            bool: True if the processor is from Intel, False otherwise.
+        """
+        return 'intel' in platform.processor().lower()
