a987f241d0a3535eaccc1641eba69fa00479593b
erik <erik172dev@gmail.com>
2024-12-20 21:35:44 -0500

    Update DEBUG configuration to default to False in app settings

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 
 def configurer_app(app) -> None:
-    app.config['DEBUG'] = os.getenv('DEBUG', True)
+    app.config['DEBUG'] = os.getenv('DEBUG', False)
     app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
